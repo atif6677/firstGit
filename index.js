68f125c62097899fa004a0593b7cea45f328151e
atif6677 <mohd.atif2531@gmail.com>
2024-11-11 02:51:59 +0530

    Creating Elements & DOM Relations

--- index.js
+++ index.js
@@ -1,49 +1,37 @@
 // Write your code below:
-const mainHeading = document.querySelector('#main-heading');
-mainHeading.style.textAlign = 'right';
+const head = document.createElement('h3');
+
+const headText = document.createTextNode('Buy high quality organic fruits online')
+
+head.appendChild(headText);
+
+head.style.fontStyle = 'italic'
+
+const divs = document.getElementsByTagName('div');
+
+const firstDiv = divs[0];
+
+firstDiv.appendChild(head)
 
 
-const fruits = document.querySelector('.fruits');
-
-fruits.style.backgroundColor = 'grey';
-fruits.style.padding = '30px';
-fruits.style.margin = '30px';
-fruits.style.width = '50%';
-fruits.style.borderRadius = '5px';
-fruits.style.listStyleType = 'none';
-
-const basketHeading = document.querySelector('#basket-heading');
-basketHeading.style.marginleft = '30px';
-basketHeading.style.color = 'brown';
 
 
-const fruitItem = document.querySelectorAll('.fruit');
-
-for (let i = 0; i < fruitItem.length; i++) {
-
-    fruitItem[i].style.backgroundColor = 'white'
-        fruitItem[i].style.padding = '10px'
-        fruitItem[i].style.margin = '10px'
-        fruitItem[i].style.borderRadius = '5px'
 
 
-}
 
 
-const OddFruitItems = document.querySelectorAll('.fruit:nth-child(odd)');
 
-for (let j = 0; j < OddFruitItems.length; j++)
-{
-    OddFruitItems[j].style.backgroundColor='lightgrey'
-}
+const headr = document.createElement('p');
 
+const headrText = document.createTextNode('Total fruits:4')
 
-// Write answer to the questions asked below:
+headr.appendChild(headrText);
 
-const EvenFruitItems = document.querySelectorAll('.fruit:nth-child(even)');
+const divsr = document.getElementsByTagName('div');
 
-for (let j = 0; j < EvenFruitItems.length; j++)
-{
-    EvenFruitItems[j].style.backgroundColor='brown'
-    EvenFruitItems[j].style.color='white'
-}
+const secondDiv = divsr[1];
+
+const fruits =document.querySelector('.fruits')
+secondDiv.insertBefore(headr, fruits);
+
+headr.id = 'fruits-total'
